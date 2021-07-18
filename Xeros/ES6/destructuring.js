8e3ce83a17f4236d714e04630da45858659741b9
XerosLab <xeros.log@gmail.com>
2021-07-19 00:11:43 +0900

    destructuring object까지 완료

---
+++ Xeros/ES6/destructuring.js
@@ -0,0 +1,21 @@
+// Destructuring
+// Array
+let data = ["crong", "honux", "jk", "jinny"];
+// let jisu = data[0];
+// let jung = data[2];
+let [jisu, , jung] = data;
+
+console.log(jisu, jung);
+
+// Object
+let obj = {
+    name: "crong",
+    address: "Korea",
+    age:10
+}
+
+// let { name, age } = obj;
+// console.log(name, age);
+
+let { name: myName, age: myAge } = obj;
+console.log(myName, myAge);
