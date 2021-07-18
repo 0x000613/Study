6f8ac92ddc3829a57e67027d94f60a3dae7e530e
XerosLab <xeros.log@gmail.com>
2021-07-18 22:41:36 +0900

    git ignore added

---
+++ Xeros/ES6/createObj.js
@@ -0,0 +1,17 @@
+const name = "crong";
+const age = 33;
+
+const obj = {
+    name: name,
+    age: age
+}
+
+console.log(obj)
+
+
+function getObj() {
+    const name = "crong";
+    const getName = function () {
+        return name;
+    }
+}
