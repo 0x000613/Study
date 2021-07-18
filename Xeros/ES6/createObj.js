426d246595498d200cdbf5df78f24948f8f7316f
XerosLab <xeros.log@gmail.com>
2021-07-18 23:02:02 +0900

    createObj 강의 예제 코드 완료

--- Xeros/ES6/createObj.js
+++ Xeros/ES6/createObj.js
@@ -1,17 +1,46 @@
-const name = "crong";
-const age = 33;
+// const name = "crong";
+// const age = 33;
 
-const obj = {
-    name: name,
-    age: age
-}
+// const obj = {
+//     name: name,
+//     age: age
+// }
 
-console.log(obj)
+// console.log(obj)
 
 
 function getObj() {
     const name = "crong";
+    
     const getName = function () {
         return name;
     }
+
+    const setName = function (newname) {
+        name = newname;
+    }
+
+    const printName = function () {
+        console.log(name)
+    }
+
+    // return {
+    //     getName: getName,
+    //     setName: setName
+    // }
+    // 위 코드구문을 조금 더 간소화
+    return {getName, setName}
 }
+
+var obj = getObj();
+console.log(obj.getName());
+
+
+// 간단한 object literal 선언 방법
+// const data = {
+//     name,
+//     getName() {
+
+//     },
+//     age
+// }
