fb8ffa468890f0ca722d47c46374ba8f6972a079
XerosLab <xeros.log@gmail.com>
2021-07-19 23:55:52 +0900

    destructuring 챕터 완료

--- README.md
+++ README.md
@@ -74,9 +74,8 @@
     => 임시 중단 (모던 JavaScript 튜토리얼 먼저 진행)
   
    [(모던 JavaScript 튜토리얼)](https://ko.javascript.info/)
-  
   - 2021-07-19 ~ 2021-07-26  
-    => ?
+    => 섹션 1-1 ~ 섹션 2.18
   
 - 이현수
   HTML / CSS 강의 수강중 [(TCP School)](http://tcpschool.com/html/intro) / [(goorm edu)](https://edu.goorm.io/learn/lecture/26571/%EC%A0%80%EC%9E%90-%EC%A7%81%EA%B0%95-do-it-html-css-js-%EC%9B%B9-%ED%91%9C%EC%A4%80%EC%9D%98-%EC%A0%95%EC%84%9D)
--- Xeros/ES6/destructuring.js
+++ Xeros/ES6/destructuring.js
@@ -14,8 +14,48 @@ let obj = {
     age:10
 }
 
+// 아래 코드는 obj에서 선언된 변수명 그 자체를 변경하지 않고 바로 사용 할 경우
 // let { name, age } = obj;
 // console.log(name, age);
 
+// 아래 코드는 obj에서 선언된 변수명 대신 새로운 변수명을 선언한 뒤 값을 할당 ( {오브젝트 내 선언된 변수명:새로 선언될 변수명} )
 let { name: myName, age: myAge } = obj;
 console.log(myName, myAge);
+
+
+// Json 형태의 데이터 파싱
+var news = [
+    {
+        "title": "sbs",
+        "imgurl": "https://sbs.com",
+        "newslist": [
+            "sbs1",
+            "sbs2",
+            "sbs3"
+        ]
+    },
+    {
+        "title": "mbc",
+        "imgurl": "https://mbc.com",
+        "newslist": [
+            "mbc1",
+            "mbc2",
+            "mbc3"
+        ]
+    }
+];
+
+// let [, mbc] = news;
+// let { title, imgurl } = mbc;
+// console.log(title, imgurl)
+
+// // 위 코드를 조금 더 간소화하는 방법
+let [, { title, imgurl }] = news;
+console.log(title, imgurl)
+
+// _Event객체 전달
+function getNewsList([{ newslist }]) {
+    console.log(newslist);
+}
+
+getNewsList(news);
